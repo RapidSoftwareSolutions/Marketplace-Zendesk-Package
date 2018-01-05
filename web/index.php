f847c45bc9efc0c1ae639b1df12a7a05c333bc2c
ivz-dev <zakutynsky@yahoo.com>
2018-01-05 17:51:22 +0200

    test

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL);
-ini_set("display_errors", 1);
+ini_set("display_errors", 0);
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../app/AppKernel.php';
@@ -9,5 +9,6 @@ use Symfony\Component\HttpFoundation\Request;
 $kernel = new AppKernel('prod', false);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
+exit();
 $response->send();
 $kernel->terminate($request, $response);
